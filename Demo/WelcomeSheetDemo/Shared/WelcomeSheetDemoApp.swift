803c0404f147d70edc820775cc69a5a611ab8cf3
Jakub Florek <63607600+MAJKFL@users.noreply.github.com>
2021-11-27 19:32:09 +0100

    Added demo

---
+++ Demo/WelcomeSheetDemo/Shared/WelcomeSheetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeSheetDemoApp.swift
+//  Shared
+//
+//  Created by Jakub Florek on 27/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct WelcomeSheetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
