fe2b07dd610c0a2fccebf3eeddb04fb03b34299d
Jakub Florek <63607600+MAJKFL@users.noreply.github.com>
2021-11-27 19:28:28 +0100

    project init

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "WelcomeSheet",
             dependencies: []),
-        .testTarget(
-            name: "WelcomeSheetTests",
-            dependencies: ["WelcomeSheet"]),
     ]
 )
